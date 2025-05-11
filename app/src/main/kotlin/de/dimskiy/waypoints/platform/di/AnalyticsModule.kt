3b1d037afcf7955013415cdd6e3541f5c72a08de
Vadim Sinitskiy <vadim.sinitskiy@gmail.com>
2025-05-11 20:58:31 +0200

    Disable Firebase when debug

--- app/src/main/kotlin/de/dimskiy/waypoints/platform/di/AnalyticsModule.kt
+++ app/src/main/kotlin/de/dimskiy/waypoints/platform/di/AnalyticsModule.kt
@@ -9,8 +9,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import de.dimskiy.waypoints.BuildConfig
 import de.dimskiy.waypoints.domain.providers.ReportingProvider
 import de.dimskiy.waypoints.platform.analytics.FirebaseReportingProvider
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
@@ -25,9 +27,21 @@ abstract class AnalyticsModule {
 
         @Provides
         fun provideFirebaseAnalytics(@ApplicationContext context: Context): FirebaseAnalytics =
-            FirebaseAnalytics.getInstance(context)
+            FirebaseAnalytics.getInstance(context).apply {
+                val isEnabled = !BuildConfig.DEBUG
+                setAnalyticsCollectionEnabled(isEnabled)
+                if (!isEnabled) {
+                    Timber.d("FirebaseAnalytics disabled in '${BuildConfig.BUILD_TYPE}' build")
+                }
+            }
 
         @Provides
-        fun provideFirebaseCrashlytics(): FirebaseCrashlytics = FirebaseCrashlytics.getInstance()
+        fun provideFirebaseCrashlytics(): FirebaseCrashlytics =
+            FirebaseCrashlytics.getInstance().apply {
+                isCrashlyticsCollectionEnabled = !BuildConfig.DEBUG
+                if (!isCrashlyticsCollectionEnabled) {
+                    Timber.d("FirebaseCrashlytics disabled in '${BuildConfig.BUILD_TYPE}' build")
+                }
+            }
     }
 }
